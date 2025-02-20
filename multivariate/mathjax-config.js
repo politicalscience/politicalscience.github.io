2b3b6b95ee6c96746a6b6a0099e0c5f075853bbf
kevinlingfeng <kevinlingfengli@outlook.com>
2025-02-20 22:56:06 +0000

    update verything

---
+++ multivariate/mathjax-config.js
@@ -0,0 +1,12 @@
+MathJax = {
+    chtml: {
+        scale: 1,
+        mtextInheritFont: true,
+        mtextFont: 'Cambria',
+        displayAlign: 'left',        // default for indentalign when set to 'auto'
+        displayIndent: '1em'
+    },
+    startup: {
+        typeset: true
+    }
+  };
