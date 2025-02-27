320ccc10661947f9b4c537683d09a1cc50e108a2
kevinlingfeng <kevinlingfengli@outlook.com>
2025-02-27 00:02:58 +0000

    update notes

--- docs/mathjax-config.js
+++ docs/mathjax-config.js
@@ -1,10 +1,18 @@
 MathJax = {
+    tex: {
+      macros: {
+        E: "\\mathbb{E}",
+        V: "\\mathbb{V}",
+        P:"\\mathbb{P}",
+        eps: "\\varepsilon"
+      }
+    },
     chtml: {
-        scale: 1,
+        scale: 1.05,
         mtextInheritFont: true,
         mtextFont: 'Cambria',
-        displayAlign: 'left',        // default for indentalign when set to 'auto'
-        displayIndent: '1em'
+        displayAlign: 'left',
+        displayIndent: '1.5em'
     },
     startup: {
         typeset: true
